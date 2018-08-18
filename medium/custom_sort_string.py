9b7dfa7ee39c2285aaf6e1b08b4fa1629d897714
向尚平 <rory@adeMBP-2.lan>
2018-08-18 13:08:40 +0800

    add custom_sort_string

---
+++ medium/custom_sort_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+"""
+class Solution:
+    def customSortString(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: str
+        """
+        s_list = list(S)
+        t_list = list(T)
+        out_list = []
+        for one in s_list:
+            while one in t_list:
+                out_list.append(one)
+                t_list.remove(one)
+        out_list.extend(t_list)
+        return "".join(out_list)
+
